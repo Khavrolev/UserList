1325a0a51927e23c5a3716f71f5c1fdab2a356b6
Khavrolev <khavrolev@gmail.com>
2021-03-25 15:10:09 +0300

    задача

--- UserList/Program.cs
+++ UserList/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace UserList
 {
@@ -6,7 +8,41 @@ namespace UserList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var users = new List<User>();
+
+            users.Add(new User("Khavrolev", "Хавролев", true));
+            users.Add(new User("Kuzsdf", "Анатолий", false));
+            users.Add(new User("g4", "Василий", true));
+            users.Add(new User("sdffs", "Артем", false));
+            users.Add(new User("sdf", "Игорь", false));
+
+            foreach (User user in users)
+            {
+                if (user.IsPremium)
+                    Console.WriteLine($"Добрый день, {user.Name}");
+                else
+                {
+                    Console.WriteLine($"Добрый день, {user.Name}, посмотрите рекламу");
+                    ShowAds();
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 }
---
+++ UserList/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserList
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User (string login, string name, bool premium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = premium;
+        }
+    }
+}
