5e084a4902a2a6a088f402ac33316eeaaa520459
Khavrolev <khavrolev@gmail.com>
2021-03-25 14:49:05 +0300

    Добавьте файлы проекта.

---
+++ UserList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
